76fc89b1fab5141e897514e9b055d41b29cbf33b
Raza <razarajpoot99598@gmail.com>
2023-10-25 15:43:50 +0500

    Home section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import "../style/Home.scss";
+import vg from "../assets/2.webp"
 
 const Home = ()=>{
     return(
-        <div>Home</div>
+        <>
+        <div className="home">
+            <main>
+                <h1>Super Dev</h1>
+                <p>Solution to all you problems</p>
+            </main>
+        </div>
+        <div className="home2">
+        <img src={vg} alt="graphics" />
+
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quidem fugiat aut ea repudiandae alias officiis voluptates optio dignissimos voluptate ad eius, vel esse saepe provident iusto impedit ullam debitis?
+            </p>
+        </div>
+        
+        </div>
+
+        </>
     );
 }
 
